bce3578d478dc7edff3b412ae5bfe369152b583b
Juano <juanosierra2@gmail.com>
2024-04-16 17:07:56 -0500

    Correccion de eliminacion de etiquetas en el eje X que no corresponden

--- limpiar.py
+++ limpiar.py
@@ -4,9 +4,9 @@ import numpy as np
 import string
 
 
-def eliminar_puntuacion(mini):
+def eliminar_puntuacion_y_stopwords(mini):
     # Definir una lista de stop words
-    stopwords = ['de', 'la', 'los', 'el', 'las', 'un']
+    stopwords = ['de', 'la', 'los', 'el', 'las', 'un', 'y', 'al']
     
     # Remover puntuación usando la tabla de traducción de str.translate
     tabla_puntuacion = str.maketrans('', '', string.punctuation)
@@ -21,12 +21,15 @@ def eliminar_puntuacion(mini):
 # Abrir el archivo en modo lectura
 with open('libro2.txt', 'r', encoding='utf-8') as f:
     # Leer el archivo línea por línea
+    palabras = []
+    conteo_palabras = {}
     for line in f:
-        palabras = [palabra.lower() for palabra in line.split()]
-        conteo_palabras = {}
-        for palabra in palabras:
-            mini_sin_puntuacion = eliminar_puntuacion(palabra)
-            conteo_palabras[mini_sin_puntuacion] = conteo_palabras.get(mini_sin_puntuacion, 0) + 1
+        line_palabras = line.split()
+        for palabra in line_palabras:
+            palabra_limpia = eliminar_puntuacion_y_stopwords(palabra)
+            if palabra_limpia:
+                palabras.append(palabra_limpia)
+                conteo_palabras[palabra_limpia] = conteo_palabras.get(palabra_limpia, 0) + 1
 
 #print(len(conteo_palabras))
 palabras = list(conteo_palabras.keys())
